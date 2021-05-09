749437558c3e16daec98be840b5d390a429c77cd
Duongh399 <71016226+Duongh399@users.noreply.github.com>
2021-05-09 11:30:22 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var vue = new Vue({
+    el:'#app,#app-2',
+    data(){
+        return{          
+            message1 : "Em bảo bố em hút rất nhiều thuốc ?",
+            message2 : "Cái này là phần giải thích đó bro"
+        }
+    }
+    metho
+
+});
